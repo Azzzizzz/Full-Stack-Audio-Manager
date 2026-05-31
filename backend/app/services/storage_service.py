971b7fec5050357306e479a83a7c7ec2dadca7a2
Syed Abdul Aziz <syedaziz9999@gmail.com>
2026-06-01 02:21:38 +0530

    Force SigV4 on S3 client so presigned URLs use regional endpoint

--- backend/app/services/storage_service.py
+++ backend/app/services/storage_service.py
@@ -1,6 +1,7 @@
 from typing import IO
 
 import boto3
+from botocore.config import Config
 from botocore.exceptions import ClientError
 
 from app.core.config import settings
@@ -11,6 +12,7 @@ def _make_client():
         region_name=settings.AWS_REGION,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        config=Config(signature_version="s3v4"),
     )
     if settings.S3_ENDPOINT_URL:
         kwargs["endpoint_url"] = settings.S3_ENDPOINT_URL
